0bc39d54452959f3dc1f3d58703b100d23cdc22d
justpliz <abay9508@gmail.com>
2023-01-08 00:25:53 +0600

    fix fallback_controller

--- lib/kursonli_kurs_web/controllers/fallback_controller.ex
+++ lib/kursonli_kurs_web/controllers/fallback_controller.ex
@@ -15,39 +15,15 @@ defmodule KursonliKursWeb.FallbackController do
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
-    |> put_view(MyErrorView)
+    |> put_view(ErrorView)
     |> render("404.html")
   end
 
   def call(conn, {:error, :unauthorized}) do
     conn
     |> put_status(403)
-    |> put_view(MyErrorView)
+    |> put_view(ErrorView)
     |> render(:"403")
-  end
-
-  # enhanced error
-  def call(conn, {:enhanced_error, error_opts}) do
-    conn
-    |> put_status(error_opts[:status_code] || 422)
-    |> put_view(ErrorView)
-    |> render("enhanced_error.json", %{error: error_opts})
-  end
-
-  # changeset validation errors
-  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
-    conn
-    |> put_status(:unprocessable_entity)
-    |> put_view(ErrorView)
-    |> render("changeset_error.json", changeset: changeset)
-  end
-
-  # enhanced error handler
-  def call(conn, {:error2, error}) do
-    conn
-    |> put_status(:unprocessable_entity)
-    |> put_view(ErrorView)
-    |> render("error2.json", error: error)
   end
 
   def call(conn, error) do
