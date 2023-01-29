72bf6f736be99216e9d7214113a2abd9fb0563f1
chudickgumanoid <sashaq0708@gmail.com>
2023-01-29 19:09:12 +0600

    del nil fun

--- lib/kursonli_kurs/context/courses.ex
+++ lib/kursonli_kurs/context/courses.ex
@@ -51,8 +51,4 @@ defmodule KursonliKurs.Context.Courses do
     c = from(c in Currency, select: %{name: c.name, short_name: c.short_name})
     from(query, preload: [currency: ^c])
   end
-
-  def courses_for_index() do
-    
-  end
 end
