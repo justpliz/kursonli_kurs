17de8e4ce9ca0695c440e9b0c753fdb9eec331ff
Artem <artem.kalachev.2002@gmail.ru>
2023-03-10 15:04:04 +0600

    feat: (KO-38) confirm delete chat

--- assets/js/gettext.js
+++ assets/js/gettext.js
@@ -1,32 +1,30 @@
-import translationsRU from '../../priv/gettext/rus/LC_MESSAGES/default.txt';
-import translationsKZ from '../../priv/gettext/kaz/LC_MESSAGES/default.txt';
+import translationsRU from "../../priv/gettext/rus/LC_MESSAGES/default.txt";
+import translationsKZ from "../../priv/gettext/kaz/LC_MESSAGES/default.txt";
 
 class Gettext {
   constructor(objectLoceles) {
-    this.locales = objectLoceles
+    this.locales = objectLoceles;
   }
   gettext(str) {
-    const lang = localStorage.getItem("lang") || "rus"
-    const regex = new RegExp(`\msgid\\s"${str}"\nmsgstr\\s"\.+"`)
-    const message = this.locales[lang].source.match(regex)
-    console.log(message)
+    const lang = localStorage.getItem("lang") || "rus";
+    const regex = new RegExp(`\msgid\\s"${str}"\nmsgstr\\s"\.+"`);
+    const message = this.locales[lang].source.match(regex);
     if (message != null) {
-      return message[0].replace(/msgid.+\nmsgstr\s"/, "").replace(/"$/, "")
-    }
-    else {
-      return str
+      return message[0].replace(/msgid.+\nmsgstr\s"/, "").replace(/"$/, "");
+    } else {
+      return str;
     }
   }
 }
 const gt = new Gettext({
-  "kaz": {
-    source: translationsKZ
+  kaz: {
+    source: translationsKZ,
   },
-  "rus": {
-    source: translationsRU
-  }
-})
+  rus: {
+    source: translationsRU,
+  },
+});
 
 export function gettext(str) {
-  return gt.gettext(str)
+  return gt.gettext(str);
 }
