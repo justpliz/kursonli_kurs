53ab62f3a7bcd5b1d307891c4f6d4bfe79e70aa4
justpliz <abay9508@gmail.com>
2023-03-30 14:22:03 +0600

    int -> float

--- lib/kursonli_kurs_web/controllers/page_controller.ex
+++ lib/kursonli_kurs_web/controllers/page_controller.ex
@@ -16,6 +16,8 @@ defmodule KursonliKursWeb.PageController do
       city_list = get_count_city_with_active_filials()
       currency_list = Currencies.all() |> Enum.map(&%{short_name: &1.short_name})
       courses_list = Filials.get_filial_by_city(city.id)
+      # |> IO.inspect()
+      # |> check_true_diapason(scrapped_list)
 
       conn
       |> render("index.html",
@@ -38,18 +40,25 @@ defmodule KursonliKursWeb.PageController do
       photo_path = "http://#{conn.host}:#{conn.port}/#{setting.photo}"
       logo_path = "http://#{conn.host}:#{conn.port}/#{setting.logo}"
 
-      conn
-      |> render("personal_page.html",
-        setting: setting,
-        filial: filial,
-        city_name: city.name,
-        city_eng_name: city.eng_name,
-        courses_list: courses_list,
-        x_coord: x_coord,
-        y_coord: y_coord,
-        photo_path: photo_path,
-        logo_path: logo_path
-      )
+      case setting.subdomen do
+        "" ->
+          conn
+          |> render("personal_page.html",
+            setting: setting,
+            filial: filial,
+            city_name: city.name,
+            city_eng_name: city.eng_name,
+            courses_list: courses_list,
+            x_coord: x_coord,
+            y_coord: y_coord,
+            photo_path: photo_path,
+            logo_path: logo_path
+          )
+
+        _any ->
+          conn
+          |> redirect(to: "#{setting.sudomen}")
+      end
     end
   end
 
@@ -69,7 +78,7 @@ defmodule KursonliKursWeb.PageController do
     |> Enum.sort_by(&(&1.name == "Алматы"), :desc)
   end
 
-  defp scraping() do
+  def scraping() do
     with {:ok, response} = Application.get_env(:kursonli_kurs, :scrapped) |> HTTPoison.get() do
       [usd_buy, eur_buy, rub_buy, _, _, _, _] =
         Regex.scan(~r/<td .+"buy .+">.+<\/td>/, response.body)
@@ -86,18 +95,18 @@ defmodule KursonliKursWeb.PageController do
       [
         %{
           currency: "USD",
-          buy: Decimal.add(usd_buy, "0.5") |> Decimal.to_string(),
-          sale: Decimal.add(usd_sale, "-0.5") |> Decimal.to_string()
+          buy: Decimal.add(usd_buy, "0.5") |> Decimal.to_string() |> rounding_str,
+          sale: Decimal.add(usd_sale, "-0.5") |> Decimal.to_string() |> rounding_str
         },
         %{
           currency: "EUR",
-          buy: Decimal.add(eur_buy, "0.5") |> Decimal.to_string(),
-          sale: Decimal.add(eur_sale, "-0.5") |> Decimal.to_string()
+          buy: Decimal.add(eur_buy, "0.5") |> Decimal.to_string() |> rounding_str,
+          sale: Decimal.add(eur_sale, "-0.5") |> Decimal.to_string() |> rounding_str
         },
         %{
           currency: "RUB",
-          buy: Decimal.add(rub_buy, "0.05") |> Decimal.to_string(),
-          sale: Decimal.add(rub_sale, "-0.05") |> Decimal.to_string()
+          buy: Decimal.add(rub_buy, "0.05") |> Decimal.to_string() |> rounding_str,
+          sale: Decimal.add(rub_sale, "-0.05") |> Decimal.to_string() |> rounding_str
         }
       ]
     end
@@ -111,5 +120,80 @@ defmodule KursonliKursWeb.PageController do
   def instruction_kaz(conn, _params) do
     conn
     |> redirect(to: "/pdfs/instruction_kurs-online_kaz.pdf")
+  end
+
+  def find_best_courses(courses) do
+    courses =
+      courses
+      # |> delete_old_courses
+      |> Enum.map(&[&1.course, &1.date, &1.filial_id])
+
+    {to_delete, _} =
+      courses
+      |> Enum.reduce({[], []}, fn [courses, _date, _id], {to_delete, untouch} ->
+        Enum.reduce(courses, {to_delete, untouch}, fn
+          %{short_name: name}, {to_delete, untouch} ->
+            if name in to_delete and name not in untouch do
+              {List.delete(to_delete, name), [name | untouch]}
+            else
+              {[name | to_delete], untouch}
+            end
+        end)
+      end)
+
+    courses_list =
+      Enum.reduce(courses, [], fn [courses, date, id], acc ->
+        [
+          [
+            Enum.reduce(courses, [], fn
+              %{short_name: name} = course, acc ->
+                if name in to_delete do
+                  acc
+                else
+                  [course | acc]
+                end
+            end),
+            date,
+            id
+          ]
+          | acc
+        ]
+      end)
+      |> Enum.map(fn [course, date, id] -> course |> Enum.map(&Map.put(&1, :id, id)) end)
+  end
+
+  def check_true_diapason(courses, scrapped) do
+    usd_scrapped = Enum.find(scrapped, &(&1.currency == "USD"))
+    usd_purchase = usd_scrapped.buy |> IO.inspect() |> String.to_float()
+    usd_sale = usd_scrapped.sale |> String.to_float()
+
+    # eur_scrapped = Enum.find(scrapped, &(&1.currency == "EUR"))
+    # eur_sale = eur_scrapped.value_for_sale |> String.to_float()
+    # eur_purchase = eur_scrapped.value_for_purchase |> String.to_float()
+
+    # rub_scrapped = Enum.find(scrapped, &(&1.currency == "RUB"))
+    # rub_sale = rub_scrapped.value_for_sale |> String.to_float()
+    # rub_purchase = rub_scrapped.value_for_purchase |> String.to_float()
+
+    courses
+    |> Enum.map(
+      &Enum.filter(&1.course, fn x ->
+        IO.inspect(x)
+        case x.short_name do
+          "USD" ->
+            value_for_sale = x.value_for_sale |> String.to_float()
+            value_for_purchase = x.value_for_purchase |> String.to_float()
+
+            if value_for_sale > usd_sale and value_for_sale < usd_purchase and
+                 value_for_purchase > usd_sale and value_for_purchase < usd_purchase do
+              &1
+            else
+              Map.put(&1.setting, :visible_website_status, false)
+            end
+            _any -> x
+        end
+      end)
+    )
+    # |> IO.inspect()
   end
 end
